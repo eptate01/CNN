bc7dc870cf5fa3e908ae47fdc172bd49bcc8f1f6
eptate01 <ethanptate@gmail.com>
2023-11-02 10:36:06 -0400

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -28,6 +28,8 @@ model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
 model.add(MaxPooling2D())
 model.add(Conv2D(32, (3,3), 1, activation='relu'))
 model.add(MaxPooling2D())
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
 model.add(Conv2D(16, (3,3), 1, activation='relu'))
 model.add(MaxPooling2D())
 #convert to a 1d tensor
